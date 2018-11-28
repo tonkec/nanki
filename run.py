6c4ca53edaae35feec8e58754597994770b8ac1e
GarethS3 <gareth.gs13@gmail.com>
2018-11-28 16:22:03 +0100

    wip

--- run.py
+++ run.py
@@ -17,9 +17,38 @@ hots=os.getenv('IP'), port=os.getenv('PORT')
 
 from flask import Flask, render_template, request, redirect
 from datetime import datetime
+import random
 import json
 
+
 app = Flask(__name__)
+
+
+class Player:
+    """Creates player profile for each new user logged"""
+    def __init__(self, username):
+        self.name = username
+        self.score = 0
+
+
+def get_random_kana():
+    """Selects the character to guess"""
+    with open('data/syllabary.json', 'r') as json_data:
+        kanas = json.load(json_data)
+    print("kanas : ", kanas)
+    return kanas
+
+
+def check_answer(answer, guess, score):
+    """Compare answer and selected character"""
+    if answer == guess['name']:
+        print('Right answer')
+        score += 1
+        print(score)
+    if answer != guess['name']:
+        print('Wrong answer')
+        score -= 2
+        print(score)
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -34,20 +63,36 @@ def index():
 
 @app.route('/log/<username>')
 def user_page(username):
-    return render_template("quiz-sel.html", user=username)
+    """
+    Quiz selection page, with logic to select 20 random characters from selected syllabary
+    """
+    player = Player(username)
+
+    print(player.name + "'s score : " + str(player.score))
+
+    return render_template("quiz-sel.html", player=player)
 
 
-@app.route('/log/<username>/<kana>')
+@app.route('/log/<username>/<kana>', methods=["GET", "POST"])
 def quiz(username, kana):
-    with open('data/syllabary.json', 'r') as kanji_data:
-        kanjis = json.load(kanji_data)
-        guess = kanjis[0]
+    """
+    Quiz main page
+    """
+    selection = get_random_kana()
+    guess = selection[0]
+    score = 0
+
+    if request.method == "POST":
+        answer = request.form["answer"]
+        check_answer(answer, guess, score)
+        return render_template('quiz.html', user=username, guess=guess, kana=kana)
 
     return render_template('quiz.html', user=username, guess=guess, kana=kana)
 
 
 @app.route('/leader-board')
-def leaderboard():
+def leader_board():
+    """route to leaderboard with top 10 scores"""
     return render_template('leader-board.html')
 
 
